1250e4000fa963287af6c8804f4bdd84f411ef17
jgarzon97 <jorgegarzonizquierdo@gmail.com>
2023-09-06 00:31:49 -0500

    resuelto el problema de comunicación entre Angular y Node.js

--- src/app/components/dashboard/inicio/inicio.component.ts
+++ src/app/components/dashboard/inicio/inicio.component.ts
@@ -1,25 +1,6 @@
 import { Component } from '@angular/core';
-
-
-export interface PeriodicElement {
-  mesa: string;
-  position: number;
-  capacidad: number;
-  estado: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, mesa: 'Mesa 1', capacidad: 4, estado: 'Libre'},
-  {position: 2, mesa: 'Mesa 2', capacidad: 4, estado: 'Libre'},
-  {position: 3, mesa: 'Mesa 3', capacidad: 6, estado: 'Libre'},
-  {position: 4, mesa: 'Mesa 4', capacidad: 6, estado: 'Libre'},
-  {position: 5, mesa: 'Mesa 5', capacidad: 10, estado: 'Libre'},
-  {position: 6, mesa: 'Mesa 6', capacidad: 10, estado: 'Libre'},
-  {position: 7, mesa: 'Mesa 7', capacidad: 4, estado: 'Libre'},
-  {position: 8, mesa: 'Mesa 8', capacidad: 4, estado: 'Libre'},
-  {position: 9, mesa: 'Mesa 9', capacidad: 2, estado: 'Libre'},
-  {position: 10, mesa: 'Mesa 10', capacidad: 2, estado: 'Libre'},
-];
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { PruebaService } from 'src/app/services/prueba.service';
 
 @Component({
   selector: 'app-inicio',
@@ -27,7 +8,18 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./inicio.component.css']
 })
 
+
 export class InicioComponent {
-  displayedColumns: string[] = ['position', 'mesa', 'capacidad', 'estado'];
-  dataSource = ELEMENT_DATA;
+
+  displayedColumns: string[] = ['numero_de_mesa', 'capacidad', 'estado'];
+  dataSource: any[] = [];
+
+  constructor(private snackBar: MatSnackBar, private pruebaService: PruebaService) {}
+
+  ngOnInit(): void {
+    this.pruebaService.getMesas().subscribe(data => {
+      console.log(data); // Verifica si estás recibiendo datos y su estructura
+      this.dataSource = data;
+    });
+  }
 }
--- src/app/components/dashboard/pedidos/pedidos.component.ts
+++ src/app/components/dashboard/pedidos/pedidos.component.ts
@@ -1,53 +1,31 @@
 import { Component, ViewChild } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatTable } from '@angular/material/table';
+import { PruebaService } from 'src/app/services/prueba.service';
 
-export interface PeriodicElement {
-  mesa: string;
-  num_pedido: number;
-  capacidad: number;
-  estado: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {num_pedido: 1, mesa: 'Mesa 1', capacidad: 4, estado: 'Ocupado'},
-  {num_pedido: 2, mesa: 'Mesa 2', capacidad: 4, estado: 'Ocupado'},
-  {num_pedido: 3, mesa: 'Mesa 3', capacidad: 6, estado: 'Ocupado'},
-  {num_pedido: 4, mesa: 'Mesa 4', capacidad: 2, estado: 'Ocupado'},
-  {num_pedido: 5, mesa: 'Mesa 5', capacidad: 4, estado: 'Ocupado'},
-  {num_pedido: 6, mesa: 'Mesa 6', capacidad: 4, estado: 'Ocupado'},
-  {num_pedido: 7, mesa: 'Mesa 7', capacidad: 6, estado: 'Ocupado'},
-  {num_pedido: 8, mesa: 'Mesa 8', capacidad: 2, estado: 'Ocupado'},
-  {num_pedido: 9, mesa: 'Mesa 9', capacidad: 4, estado: 'Ocupado'},
-  {num_pedido: 10, mesa: 'Mesa 10', capacidad: 4, estado: 'Ocupado'},
-];
 @Component({
   selector: 'app-pedidos',
   templateUrl: './pedidos.component.html',
   styleUrls: ['./pedidos.component.css']
 })
 export class PedidosComponent {
-  displayedColumns: string[] = ['num_pedido', 'mesa', 'capacidad', 'estado'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['id', 'cliente', 'fecha', 'estado', 'total'];
+  dataSource: any[] = [];
 
-  @ViewChild(MatTable) table!: MatTable<PeriodicElement>;
+  @ViewChild(MatTable) table!: MatTable<any>;
 
-  constructor(private snackBar: MatSnackBar) {}
+  constructor(private snackBar: MatSnackBar, private pruebaService: PruebaService) {}
+
+  ngOnInit(): void {
+    this.pruebaService.getDatos().subscribe(data => {
+      console.log(data); // Verifica si estás recibiendo datos y su estructura
+      this.dataSource = data;
+    });
+  }
 
   addData() {
-    if (this.dataSource.length >= 10) {
-      this.snackBar.open('No se pueden agregar más mesas.', 'Cerrar', {
-        duration: 3000, // Duración de la alerta en milisegundos
-      });
-    } else {
-      const randomElementIndex = Math.floor(Math.random() * ELEMENT_DATA.length);
-      this.dataSource.push(ELEMENT_DATA[randomElementIndex]);
-      this.table.renderRows();
-    }
   }
 
   removeData() {
-    this.dataSource.pop();
-    this.table.renderRows();
   }
 }
