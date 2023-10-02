ab91def750a0fec7e4eca2d27ea47734d3a72f54
jgarzon97 <jorgegarzonizquierdo@gmail.com>
2023-10-02 08:42:55 -0500

    formularios

--- src/app/components/dashboard/menu/nuevo-menu/nuevo-menu.component.ts
+++ src/app/components/dashboard/menu/nuevo-menu/nuevo-menu.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { ServidorService } from 'src/app/services/servidor.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ProductosService } from 'src/app/services/productos.service';
 
 @Component({
   selector: 'app-nuevo-menu',
@@ -8,11 +8,36 @@ import { ServidorService } from 'src/app/services/servidor.service';
   styleUrls: ['./nuevo-menu.component.css']
 })
 export class NuevoMenuComponent {
+  disableSelect = new FormControl(false);
+  productoForm!: FormGroup;
 
-  constructor(private servidorService: ServidorService, private router: Router) {}
+  constructor(private fb: FormBuilder, private productosService: ProductosService) { }
 
   ngOnInit(): void {
-
+    this.productoForm = this.fb.group({
+      nombre: ['', Validators.required],
+      stock: ['', Validators.required],
+      precio: ['', Validators.required],
+      tiempo: ['', Validators.required],
+      estado: '',
+      id_categoria: ['', Validators.required]
+    });
   }
 
+  onSubmit() {
+    if (this.productoForm.valid) {
+      const productData = this.productoForm.value;
+      this.productosService.createProducto(productData)
+        .subscribe(
+          (response) => {
+            // Manejar la respuesta aquí, por ejemplo, mostrar un mensaje de éxito
+            console.log('Producto creado exitosamente', response);
+          },
+          (error) => {
+            // Manejar el error aquí, por ejemplo, mostrar un mensaje de error
+            console.error('Error al crear el producto', error);
+          }
+        );
+    }
+  }
 }
